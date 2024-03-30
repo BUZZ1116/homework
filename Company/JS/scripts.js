a8bc511053e62d37619fb7b2011ab160756075a3
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2024-03-30 09:42:38 +0800

    Add files via upload

---
+++ Company/JS/scripts.js
@@ -0,0 +1,9 @@
+// script.js
+$(document).ready(function() {
+    // 在這裡添加任何需要的 JavaScript 功能
+  });
+// script.js
+$(document).ready(function() {
+    // 初始化照片輪播
+    $('.carousel').carousel();
+  });
