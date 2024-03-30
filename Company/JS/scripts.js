9c995b27604bc1b2432bdc8bb05362fa353fcd2c
BUZZ1116 <110268621+BUZZ1116@users.noreply.github.com>
2024-03-30 09:41:39 +0800

    Delete Company directory

--- Company/JS/scripts.js
+++
@@ -1,9 +0,0 @@
-// script.js
-$(document).ready(function() {
-    // 在這裡添加任何需要的 JavaScript 功能
-  });
-// script.js
-$(document).ready(function() {
-    // 初始化照片輪播
-    $('.carousel').carousel();
-  });
